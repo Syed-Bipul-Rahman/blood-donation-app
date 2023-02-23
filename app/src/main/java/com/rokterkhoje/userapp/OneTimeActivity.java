3cadcdcb2e75dbca293d576e31809c21ffbd5270
Sb-Rahman <sb.rahman@yahoo.com>
2023-02-23 23:11:19 +0600

    blood donation app first commit

---
+++ app/src/main/java/com/rokterkhoje/userapp/OneTimeActivity.java
@@ -0,0 +1,39 @@
+package com.rokterkhoje.userapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class OneTimeActivity extends AppCompatActivity {
+ImageView img1, img2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_time);
+        getSupportActionBar().hide();
+
+
+        img1=findViewById(R.id.imgOne);
+        img2=findViewById(R.id.imageView3);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(OneTimeActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(OneTimeActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/rokterkhoje/userapp/SplashActivity.java
@@ -0,0 +1,82 @@
+package com.rokterkhoje.userapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+
+        if (isFirstTime) {
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("isFirstTime", false);
+            editor.apply();
+
+            Thread thread =new Thread(){
+                public void run(){
+                    try {
+
+                        sleep(1500);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    finally {
+                        Intent intent=new Intent(SplashActivity.this , OneTimeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            };thread.start();
+
+        }else {
+            Thread thread =new Thread(){
+                public void run(){
+                    try {
+
+                        sleep(1500);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    finally {
+                        Intent intent=new Intent(SplashActivity.this , SignUpActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            };thread.start();
+            finish();
+
+        }
+
+
+//        Thread thread =new Thread(){
+//            public void run(){
+//                try {
+//
+//                    sleep(1500);
+//                }
+//                catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//                finally {
+//                    Intent intent=new Intent(SplashActivity.this , SignUpActivity.class);
+//                    startActivity(intent);
+//                }
+//            }
+//        };thread.start();
+//        finish();
+
+
+    }
+}
